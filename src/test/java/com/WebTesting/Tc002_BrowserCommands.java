cdf6f4156a8a5b2dfd52d09f9e9c26d0700845e2
PriyankaNigade18 <piyu1818@gmail.com>
2026-06-18 22:23:46 +0530

    Selenium Day 1 & 2

---
+++ src/test/java/com/WebTesting/Tc002_BrowserCommands.java
@@ -0,0 +1,58 @@
+package com.WebTesting;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
+
+public class Tc002_BrowserCommands {
+
+	public static void main(String[] args)
+	{
+		/*
+		 * Scenario:
+		 * Validate Google application title, which should be Google
+		 */
+		
+		//launch Browser: create driver session with chrome
+		WebDriver driver=new ChromeDriver();
+		
+		//open google application:get(URL)
+		driver.get("https://www.google.com/");
+		
+		//get the page title: getTitle():string
+		String actTitle=driver.getTitle();
+		
+		//validate title should be Google
+		if(actTitle.equals("GoogleApp"))
+		{
+			System.out.println("Title Match....Test Pass");
+		}else
+		{
+			System.out.println("Title not match....Test Fail");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
