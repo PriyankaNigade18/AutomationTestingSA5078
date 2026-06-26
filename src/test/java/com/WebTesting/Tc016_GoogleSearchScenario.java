0c3fda24c9c8d3f6aec384cccc0751ad08eb87db
PriyankaNigade18 <piyu1818@gmail.com>
2026-06-26 22:37:38 +0530

    Selenium Day-7

--- src/test/java/com/WebTesting/Tc016_GoogleSearchScenario.java
+++ src/test/java/com/WebTesting/Tc016_GoogleSearchScenario.java
@@ -1,10 +1,12 @@
 package com.WebTesting;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Tc016_GoogleSearchScenario {
@@ -19,7 +21,19 @@ public class Tc016_GoogleSearchScenario {
 		driver.get("https://www.google.com/");
 		
 		//search for keyword
-		driver.findElement(By.id("APjFqb")).sendKeys("Jenkins",Keys.ENTER);
+		driver.findElement(By.id("APjFqb")).sendKeys("Jenkins");
+		
+		//get the suggestions from google in console
+		List<WebElement> allOptions=driver.findElements(By.cssSelector("ul[role='listbox']>li"));
+		
+		System.out.println("Total options: "+allOptions.size());
+		
+		for(WebElement i:allOptions)
+		{
+			System.out.println(i.getText());
+		}
+		
+		
 		
 
 	}
